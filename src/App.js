58574820d283edcc3668bc565e645b3368b40581
PavelB21 <103366870+PavelB21@users.noreply.github.com>
2022-04-27 15:58:01 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import './App.css';
 import  {BrowserRouter, Routes, Route} from "react-router-dom";
 import Profile from "./Pages/Profile";
 import Feed from "./Pages/Feed";
-import Navbar from "./Ul/Navbar/Navbar";
+import Sidebar from "./components/Sidebar";
+import Post from "../src/components/Post";
 function App() {
   return (
-    <BrowserRouter>
-        <Navbar/>
-        <Routes>
-            <Route path="profile" element={<Profile/>}/>
-        </Routes>
-        <Routes>
-            <Route path="feed" element={<Feed/>}/>
-        </Routes>
-</BrowserRouter>
+  <BrowserRouter>
+      <Sidebar/>
+    <Routes>
+      <Route path="profile" element={<Profile/>}/>
+    </Routes>
+    <Routes>
+      <Route path="feed" element={<Feed/>}/>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
